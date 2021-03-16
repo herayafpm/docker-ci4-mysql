6d00352fd01f1089eb50f5bae34c9fe64533c608
herayafpm <herayafpm@gmail.com>
2021-03-16 23:59:05 +0700

    first docker codeigniter 4

---
+++ src/index.php
@@ -0,0 +1,12 @@
+
+<?php
+ 
+$host = 'db';
+$user = 'root';
+$pass = 'cod31gn1t3';
+$conn = new mysqli($host, $user, $pass);
+ 
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected to MySQL successfully!";
